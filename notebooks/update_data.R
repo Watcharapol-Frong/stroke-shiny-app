9a7d95da1299298d900ce7bbb4adf4853aff8ac5
Watcharapol Frong <frongbook@hotmail.com>
2025-05-06 12:03:24 +0700

    Create update_data.R

---
+++ notebooks/update_data.R
@@ -0,0 +1,17 @@
+library(DBI)
+library(RMySQL)
+library(readr)
+
+# Connect to AWS RDS
+con <- dbConnect(RMySQL::MySQL(),
+                host = "stroke-db.cpcsqk8849iz.ap-southeast-1.rds.amazonaws.com",
+                port = 3306,
+                user = "admin",
+                password = "Qwe12345#",
+                dbname = "stroke_project")
+
+# Read CSV
+stroke_df <- read_csv("stroke_data.csv")
+
+# Upload to MySQL (RDS)
+dbWriteTable(con, name = "stroke_data", value = stroke_df, row.names = FALSE)
